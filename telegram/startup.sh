47df77fa2e2a3eea1325682d69e708f3e8344a33
Giang Vu <giang.vu1@mservice.com.vn>
2025-06-13 23:55:17 +0700

    .

--- telegram/startup.sh
+++ telegram/startup.sh
@@ -1,6 +1,4 @@
-docker compose -f telegram/docker-compose.yaml  up --build
-# Uncomment the line below to run in detached mode
-# docker compose -f telegram/docker-compose.yaml  up --build --detach 
+docker compose -f telegram/docker-compose.yaml  up --build --detach
 
 
 # Uncomment the line below to stop and remove containers, networks, images, and volumes
