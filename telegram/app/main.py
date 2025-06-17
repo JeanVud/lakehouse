7440eadd5753d9de20094e0759b7557737b1c179
Giang Vu <giang.vu1@mservice.com.vn>
2025-06-18 00:08:24 +0700

    .

--- telegram/app/main.py
+++ telegram/app/main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=443,  # Expose on port 443 for HTTPS
+        # port=443,  # Expose on port 443 for HTTPS
         ssl_keyfile=PRIVKEY_PATH,
         ssl_certfile=FULLCHAIN_PATH,
         reload=True # Set to False for production
